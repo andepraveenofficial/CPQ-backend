e1077fe4be714a2b2e41f1eb08721f58b0e095d2
Durga Bhavani <durgabhavaniyasarla232@gmail.com>
2024-07-03 11:13:19 +0530

    implemented login API

--- src/api/v1/routes/user.route.ts
+++ src/api/v1/routes/user.route.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
 /* -----> Controllers <----- */
-import createUser from '../controllers/user.controller';
+import { createUser, loginUser } from '../controllers/user.controller';
 
 const router = express.Router();
 
 // Registration -> Create a new user
 router.post('/', createUser);
-
+router.post('/login', loginUser);
 export default router;
