de11b0e09f66c14bed2dac9cd93ebdcaa5ae6e0d
Ande Praveen <praveenande2023@gmail.com>
2024-07-22 23:39:31 +0530

    implemented api-document

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
 import path from 'path';
 import cors from 'cors';
+import swaggerOutput from '../swagger_output.json';
 import v1Routes from './api/v1';
 
 // Load environment variables
@@ -16,11 +18,15 @@ app.use(express.json()); // Handle JSON data
 app.use(cors()); // CORS
 
 /* -----> Routes <----- */
+
 // 00 Test
 app.get('/', (req: Request, res: Response) => {
   console.log('Hello World');
   res.send('Hello World');
 });
+
+// API Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 // API v1 routes
 app.use('/api/v1', v1Routes);
